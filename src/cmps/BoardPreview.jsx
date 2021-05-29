2f2c3d564f778fe96292fc6f7989116c538b1ebf
Roey13 <brdroey@gmail.com>
2021-05-29 19:52:16 +0300

    fdsfdsfds

--- src/cmps/BoardPreview.jsx
+++ src/cmps/BoardPreview.jsx
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom'
 import React from 'react';
+// import image from '../assets/imgs/b101.jpg'
 
 export function BoardPreview({ board }) {
+    const img = board.style.imgUrl
     return (
         <Link to={`/board/${board._id}`} key={board._id} style={{ textDecoration: 'none' }}>
-            <div key={board._id} className="board-preview">
+            <div key={board._id} className="board-preview"  style={{backgroundImage: `url(` + img + `)`}}>
                 <h3>{board.title}</h3>
             </div>
         </Link>
