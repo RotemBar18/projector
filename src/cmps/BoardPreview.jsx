4579bc275304db6a26323e75f66125b0a5e583d0
Roey13 <brdroey@gmail.com>
2021-05-30 23:07:14 +0300

    11PM

--- src/cmps/BoardPreview.jsx
+++ src/cmps/BoardPreview.jsx
@@ -3,10 +3,13 @@ import React from 'react';
 // import image from '../assets/imgs/b101.jpg'
 
 export function BoardPreview({ board }) {
-    const img = board.style.imgUrl
+    const img = {backgroundImage: `url(` + board.style.imgUrl + `)`}
+    const clr = {backgroundColor: board.style.bgColor}
+    let style = clr;
+    if (board.style.imgUrl) style = img
     return (
         <Link to={`/board/${board._id}`} key={board._id} style={{ textDecoration: 'none' }}>
-            <div key={board._id} className="board-preview"  style={{backgroundImage: `url(` + img + `)`}}>
+            <div key={board._id} className="board-preview"  style={style}>
                 <h3>{board.title}</h3>
             </div>
         </Link>
