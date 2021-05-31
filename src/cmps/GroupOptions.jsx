fbcb4604e5d47e589fb14125d49aebadd6593a87
Rotem Bar <rotem68907@gmail.com>
2021-05-31 22:13:31 +0300

    commitush 31//5

--- src/cmps/GroupOptions.jsx
+++ src/cmps/GroupOptions.jsx
@@ -12,11 +12,13 @@ export class GroupOptions extends Component {
 
                 <div onClick={onToggleGroupOptions} className='group-options-window'></div>
                 <div className='group-options-container'>
-                    <div className='title'>List Actions</div>
-                    <button className='close-group-options-btn' onClick={onToggleGroupOptions}>X</button>
-                    <button onClick={onToggleAddTask}>Add Card</button>
-                    <button onClick={onCopyGroup}>Copy List</button>
-                    <button onClick={onDeleteGroup}>Delete List</button>
+                    <div className='group-options-header'>
+                        <div className='title'>List Actions</div>
+                        <button className='close-group-options-btn' onClick={onToggleGroupOptions}>X</button>
+                    </div>
+                    <button onClick={onToggleAddTask}>Add Card...</button>
+                    <button onClick={onCopyGroup}>Copy List...</button>
+                    <button onClick={onDeleteGroup}>Delete List...</button>
                 </div>
             </React.Fragment>
         )
